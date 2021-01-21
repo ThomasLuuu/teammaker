81fad7d29b4181779547e0ac3701f7127846cb2a
ThomasLuuu <luutoan07@gmail.com>
2021-01-21 23:22:20 +0700

    changed alot

--- app.js
+++ app.js
@@ -3,33 +3,16 @@ const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const flash = require('connect-flash');
-const socketio = require('socket.io');
 const session = require('express-session');
 const vendors = require('vendors');
 const app = express();
-const http = require('http').Server(app);
-
+const socket = require('socket.io');
 const bodyParser = require('body-parser');
-const io = require('socket.io')(http);
 // Passport Config
 require('./config/passport')(passport);
 
 //Run connect IO
-io.sockets.on('connection', function(socket) {
-  socket.on('username', function(username) {
-      socket.username = username;
-      io.emit('is_online', '🔵 <i>' + socket.username + ' join the chat..</i>');
-  });
 
-  socket.on('disconnect', function(username) {
-      io.emit('is_online', '🔴 <i>' + socket.username + ' left the chat..</i>');
-  })
-
-  socket.on('chat_message', function(message) {
-      io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
-  });
-
-});
 // DB Config
 const db = require('./config/keys').mongoURI;
 
@@ -48,6 +31,7 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 // Express body parser
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/node_modules_bootstrap/dist/css' ));
 app.use(express.static(__dirname+ './config'));
@@ -71,7 +55,7 @@ app.use(passport.session());
 //Body Parser
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:false}));
 // Connect flash
 app.use(flash());
 
@@ -83,19 +67,33 @@ app.use(function(req, res, next) {
   next();
 });
 
-// Routes
-app.get('/chatme', function(req, res) {
-  res.render('chat.ejs');
-  
+
+//Render index page
+app.get('/chat', (req, res) => {
+  res.render('index')
 })
+
+//Get username and roomname from form and pass it to room
+app.post('/room', (req, res) => {
+  roomname = req.body.roomname;
+  username = req.body.username;
+  res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
+
+//Rooms
+app.get('/room', (req, res)=>{
+  res.render('room')
+})
+
+
 app.use('/users', require('./routes/users.js'));
 app.use('/', require('./routes/index.js'));
-const PORT = process.env.PORT ||  5000;
-// app.listen(PORT, console.log(`Server started on port ${PORT}`));
-// app.listen(process.env.PORT, function() {
-  
-// })
+const port = process.env.PORT ||  5000;
 
-const server = http.listen(PORT, function() {
-  console.log('listen on 5000')
+//Start Server
+const server = app.listen(port, () => {
+  console.log(`Server Running on ${port}`)
 })
+
+const io = socket(server);
+require('./utils/socket')(io);
