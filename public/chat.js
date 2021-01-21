d049c54301b511e4cc84ccbd57b7e95b34490e18
ThomasLuuu <luutoan07@gmail.com>
2021-01-22 00:33:02 +0700

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -6,7 +6,7 @@ const roomMessage = document.querySelector('.room-message');
 const users = document.querySelector('.users');
 
 //Socket server URL
-const socket = io.connect('localhost:5000');
+const socket = io.connect('https://team-making.herokuapp.com/');
 
 //Fetch URL Params from URL
 const queryString = window.location.search;
